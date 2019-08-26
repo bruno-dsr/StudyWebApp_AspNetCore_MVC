6487fab3674a0741e4849b56f825c1d7ce1c3084
Bruno Ramalho <bruno.dsr@hotmail.com>
2019-08-26 11:43:34 -0300

    Testes com Controller Departamentos

---
+++ VendasWebMVC/Controllers/DepartamentosController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using VendasWebMVC.Models;
+
+namespace VendasWebMVC.Controllers
+{
+    public class DepartamentosController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Departamento> departamentos = new List<Departamento>();
+
+            departamentos.Add(new Departamento(1, "Eletrônicos"));
+            departamentos.Add(new Departamento(2, "Moda"));
+
+            return View(departamentos);
+        }
+    }
+}
---
+++ VendasWebMVC/Models/Departamento.cs
@@ -0,0 +1,18 @@
+﻿namespace VendasWebMVC.Models
+{
+    public class Departamento
+    {
+        public int ID { get; set; }
+        public string Nome { get; set; }
+
+        public Departamento()
+        {
+        }
+
+        public Departamento(int iD, string nome)
+        {
+            ID = iD;
+            Nome = nome;
+        }
+    }
+}
